20332d62abec93bda57821e317bb7c5b1de87f80
Chhuong Le <77046302+ChhuongLe@users.noreply.github.com>
2021-07-28 18:05:40 -0700

    Time: 80 ms (78.88%), Space: 41.5 MB (13.65%) - LeetHub

--- two-sum/two-sum.js
+++ two-sum/two-sum.js
@@ -3,14 +3,23 @@
  * @param {number} target
  * @return {number[]}
  */
+// hash table approach
 var twoSum = function(nums, target) {
-    let result = [];
+    // set variable called numObj to be an empty hash table
+    let numObj = {};
+    // iterate through the nums array
+    for (let i = 0; i < nums.length; i++){
+        // set the key to equal the number in the array
+        let thisNum = nums[i];
+        // set the value to be the index in the nums array
+        numObj[thisNum] = i;
+    }
+    // Now iterate through the nums array and compare the difference to the hash map
     for (let i = 0; i < nums.length; i++) {
-        for (let j = 1; j <nums.length; j++) {
-            if (nums[i] + nums[j] === target && i !== j) {
-                result = [i,j];
-                return result;
-            }
+        let diff = target - nums[i];
+        // if the difference exsists inside the hash map, return it
+        if(numObj.hasOwnProperty(diff) && i !== numObj[diff]) {
+            return [i, numObj[diff]];
         }
     }
 };
