478340d70953e490eab3399a57689049c93bdf3b
Chhuong Le <77046302+ChhuongLe@users.noreply.github.com>
2021-08-08 10:57:29 -0700

    Time: 80 ms (93.48%), Space: 44.5 MB (84.78%) - LeetHub

---
+++ greatest-sum-divisible-by-three/greatest-sum-divisible-by-three.js
@@ -0,0 +1,35 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSumDivThree = function(nums) {
+    let n1 = Number.MAX_SAFE_INTEGER;
+    let n2 = Number.MAX_SAFE_INTEGER;
+    let total = 0;
+
+    for(let i = 0; i < nums.length; i++) {
+       let currNum = nums[i];
+       total += currNum;
+
+       if(currNum%3 === 1) {
+           n2 = Math.min(n2, n1+currNum);
+           n1 = Math.min(n1, currNum);
+       } 
+
+       if(currNum%3 === 2) {
+           n1 = Math.min(n1, n2+currNum);
+           n2 = Math.min(n2, currNum);
+       }
+    }
+
+    console.log(n1);
+    console.log(n2);
+
+    if(total%3 === 1) {
+        total = total - n1;
+    } else if (total%3 === 2) {
+      total = total - n2;
+    } 
+    
+    return total;
+};
