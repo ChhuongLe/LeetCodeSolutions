e9379db79d5592227f6eaca0611216974336a54e
Chhuong Le <77046302+ChhuongLe@users.noreply.github.com>
2021-08-18 11:44:49 -0700

    Time: 56 ms (99.69%), Space: 38.7 MB (54.91%) - LeetHub

---
+++ climbing-stairs/climbing-stairs.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+
+/* IOCE:
+    Input: Positive integer
+    output: number of ways to climb the stairs
+    contraints: input can only be positive
+    edge cases: none
+*/ 
+
+// using dynamic programming
+var climbStairs = function(n) {
+    // initialize array
+    // will store subproblems
+    let result = Array(n+1);
+    // base case how many ways to climb 0 steps, 1
+    result[0] = 1;
+    // how many ways to climb 1 steps, 1
+    result[1] = 1;
+    for (let i = 2; i <= n; i++) {
+        result[i] = result[i -1] + result[i-2];
+    }
+    return result[n];
+};
