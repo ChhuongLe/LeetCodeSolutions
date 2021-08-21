15b7da474a739bfac0278864566cb58011369699
Chhuong Le <77046302+ChhuongLe@users.noreply.github.com>
2021-08-21 10:33:04 -0700

    Time: 108 ms (89.01%), Space: 43.2 MB (89.65%) - LeetHub

--- coin-change/coin-change.js
+++ coin-change/coin-change.js
@@ -3,22 +3,35 @@
  * @param {number} amount
  * @return {number}
  */
+
+/* Dynamic programming problem again */
+
+/* IOCE: 
+    input: array of coins and integer amount 
+    output: integer (representing smallest number of coins to produce the amount) 
+    constraints: coins will never be negative, amount will never be negative 
+    edge cases: if array of coins cannot reach amount demonination, return -1
+*/
+
 var coinChange = function(coins, amount) {
-    // array will be initialized with a bunch of 0's
-    // fill with impossible number, amount+1 will do fine
-    let dp = new Array(amount+1).fill(amount+1);
-    // Start with smallest subproblem
-    // Amount 0, it will have 0 coins
+    // using a dyamic array we can split the problem into subproblems until the problem is satisfied
+    // since we want to have the smallest integer combination, need an impossible number to be the starting value 
+    // the dyanmic array values
+    let dp = Array(amount+1).fill(amount+1);
+    // base case, amount is 0. Need 0 coins for that
     dp[0] = 0;
     
-    // loop through until the amount is reached
+    // loop through the array until the amount is achieved
     for (let i = 0; i <= amount; i++) {
-        // use all coins to get to the amount
-        for (let j = 0; j < coins.length; j++) {
-            if(coins[j] <= i) {
-                dp[i] = Math.min(dp[i], 1 + dp[i - coins[j]]);
+        // inner loop is to loop through all the possible combinations and compare current value to min value
+        for (let j = 0; j <= coins.length; j++) {
+            if (coins[j] <= i) {
+                dp[i] = Math.min(dp[i], 1 + dp[i-coins[j]]);
             }
         }
+    } 
+    if (dp[amount] > amount) {
+        return -1;
     }
-    return dp[amount] > amount ? -1 : dp[amount];
+    return dp[amount];
 };
