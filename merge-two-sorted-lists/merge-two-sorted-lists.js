fb9af172bb713f9ebbefe877af7e01052eddd287
Chhuong Le <77046302+ChhuongLe@users.noreply.github.com>
2021-09-17 13:56:14 -0700

    Time: 117 ms (20.68%), Space: 40.2 MB (91.49%) - LeetHub

--- merge-two-sorted-lists/merge-two-sorted-lists.js
+++ merge-two-sorted-lists/merge-two-sorted-lists.js
@@ -10,41 +10,26 @@
  * @param {ListNode} l2
  * @return {ListNode}
  */
-
-/* IOCE:
-    input: 2 linked lists, sorted in non-decreasing order
-    output: 1 linked list, sorted in non-decreasing order
-    constraints: none
-    edge cases: none
-*/
- 
 var mergeTwoLists = function(l1, l2) {
-    // initalize a dummy head node
-    let mergedList = {val: Number.MIN_SAFE_INTEGER, next: null};
-    // initialize a counter variable to keep track of the current node
-    // start with the dummy head
+    // initialize another list to store the concatenated list
+    let mergedList = {val: Number.MIN_SAFE_NUMBER, next: null};
+    // initialize a ctr variable
+    // this is to keep mergedList as the head
     let ctr = mergedList;
-    // iterate through the linked lists while there are still nodes to compare 
+    // while the end of l1 or end of l2 has not been reached
+    // continue iterating through
     while (l1 && l2) {
-        // if the node in the second list is less than the node in the first list
-        if(l1.val > l2.val) {
-            // set the current node's link to l2's node
+    // if l1's val is greater
+        if (l1.val > l2. val) {
             ctr.next = l2;
-            // increment to next node in l2
             l2 = l2.next;
         } else {
-            // otherwise set the current node's link to l1
-            ctr.next = l1;
-            // increment to next node in l1
-            l1 = l1.next
+           ctr.next = l1;
+           l1 = l1.next;
         }
-        // move onto the next node
         ctr = ctr.next;
     }
-    // if one of the lists has nothing left to compare, point the list's next to the remaining nodes in the list
-    // otherwise, point to null
     ctr.next = l1 || l2;
-    
-    // return the merged linked list
+
     return mergedList.next;
 };
