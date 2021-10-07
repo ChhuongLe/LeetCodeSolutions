43d3677229b4f6848b01bb9b124c98b07ce01e70
Chhuong Le <77046302+ChhuongLe@users.noreply.github.com>
2021-10-07 14:35:07 -0700

    Time: 56 ms (99.89%), Space: 39.5 MB (79.55%) - LeetHub

---
+++ same-tree/same-tree.js
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {boolean}
+ */
+
+/*IOCE: 
+    Inputs: 2 roots of 2 binary trees
+    Outputs: boolean
+    Constraints/Complexities: number of nodes in both trees range from [0, 100]
+                             value of each node range from -10^4 <= Node.val <= 10^4
+    Edge Cases: None
+*/
+
+const isSameTree = (p, q) => {
+    // see if p and q are null, if they both are, return true
+    if (!p && !q) {
+        return true;
+    }
+    if (!p || !q) {
+        return false;
+    }
+    // otherwise, check if their subtree values are equal and in the same area
+    return (isSameTree(p.right, q.right) && isSameTree(p.left, q.left) && p.val === q.val);
+};
