0e108868d0960099cc4bcf7683f26e5cf06aefb4
Chhuong Le <77046302+ChhuongLe@users.noreply.github.com>
2021-10-26 10:38:32 -0700

    Time: 96 ms (74.86%), Space: 41.1 MB (54.96%) - LeetHub

--- set-matrix-zeroes/set-matrix-zeroes.js
+++ set-matrix-zeroes/set-matrix-zeroes.js
@@ -2,40 +2,47 @@
  * @param {number[][]} matrix
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
-
-/* 
-    Steps: 
-        1. Iterate through the matrix
-        2. Mark the column and row that contains the 0
-        3. Rerun through the matrix 
-        4. Find the marks and replace the row and columns with 0's
+/*
+    IOCE: 
+       Input: Matrix
+       Output: Matrix
+       Constraints: 
+            1. m === matrix.length
+            2. n === matrix[0].length
+            3. 1 <= m, n <= 200
+            4. All numbers are within 32 bit range
+      Edge Cases/Examples: None, example done in excalidraw
 */
 
-var setZeroes = function(matrix) {
-    // store the length of the row and column
-    let r = matrix.length;
-    let c = matrix[0].length;
+// Must be done IN PLACE
+const setZeroes = (matrix) => {
+    const m = matrix.length;
+    const n = matrix[0].length;
+
+    let row_set = new Set();
+    let col_set = new Set();
     
-    // Create a set to store the unique integers 
-    let rows = new Set();
-    let cols = new Set();
-    
-    // mark the rows and columns that are to be made 0
-    for (let i = 0; i < r; i++) {
-        for (let j = 0; j < c; j++) {
-            if (matrix[i][j]===0) {
-                rows.add(i);
-                cols.add(j);
+    // iterate through the matrix
+    // find the zeroes
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++){
+            if (matrix[i][j] === 0){
+                // remember this position, this row and column need to be zero'd out
+                row_set.add(i);
+                col_set.add(j);
             }
-        }
+        }        
     }
-    
-    //Iterate through the array again and update the elements
-    for(let i = 0; i < r; i++) {
-        for (let j = 0; j < c; j++) {
-            if(rows.has(i) || cols.has(j)) {
+    // next step is to update the matrix
+    // iterate through
+    for (let i = 0; i < m; i++) {
+        for(let j = 0; j < n; j++) {
+            // if the row has i or if the col has j
+            if (row_set.has(i) || col_set.has(j)) {
+                // update the number
                 matrix[i][j] = 0;
             }
         }
     }
-};
+    return matrix;
+}
