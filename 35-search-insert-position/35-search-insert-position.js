7962616b0d916de3ec98f29df6556cb271415f41
Chhuong Le <77046302+ChhuongLe@users.noreply.github.com>
2022-01-11 10:46:25 -0800

    Time: 105 ms (19.70%), Space: 39.5 MB (77.94%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.js
+++ 35-search-insert-position/35-search-insert-position.js
@@ -11,11 +11,25 @@
     Edge Cases: None
 */
 var searchInsert = function(nums, target) {
-    // iterate through nums
-    for (let i = 0; i < nums.length; i++) {
-        if (target === nums[i] || target < nums[i]) {
-            return i;
+    // iterate through nums, using binary search methods
+    // initialize values for the start and end pointers
+    let start = 0;
+    let end = nums.length-1;
+    // continue iterating through the nums array until start and end meet
+    while(start <= end) {
+        // set the mid point
+        let mid = Math.floor((start+end)/2);
+        // if the target is equivelent to the mid point
+        if(target === nums[mid]) {
+            // return the index
+            return mid;
+          // otherwise, shift search indexes accordingly  
+        } else if (nums[mid] > target) {
+            end = mid-1;
+        } else {
+            start = mid+1;
         }
     }
-    return nums.length;
+    // if no index is found, place the value at the end of the array of nums
+    return start;
 };
