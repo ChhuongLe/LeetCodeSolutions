81340f4d02c75325bd76633e232cdac281a8ebc1
Chhuong Le <77046302+ChhuongLe@users.noreply.github.com>
2022-01-23 21:44:38 -0800

    Time: 343 ms (9.01%), Space: 38.7 MB (92.32%) - LeetHub

---
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(numbers, target) {
+    for (let i = 0; i < numbers.length; i++){
+        for (let j = i+1; j < numbers.length; j++) {
+            if(numbers[i]+numbers[j] === target) {
+                return [i+1,j+1];
+            }
+        }
+    }
+};
