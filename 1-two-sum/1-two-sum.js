5960353a1913e2a9c3b93bbe3bb74e8ed758dd76
Chhuong Le <77046302+ChhuongLe@users.noreply.github.com>
2022-01-23 21:59:33 -0800

    Time: 97 ms (69.85%), Space: 41.1 MB (21.35%) - LeetHub

--- 1-two-sum/1-two-sum.js
+++ 1-two-sum/1-two-sum.js
@@ -11,14 +11,22 @@ IOCE:
     Edge Cases: None to consider at the moment
 */
 var twoSum = function(nums, target) {
-    let res = [];
-    for(let i = 0; i < nums.length; i++){
-        for(let j = i+1; j < nums.length; j++) {
-            if(nums[i]+nums[j] === target) {
-                res.push(i);
-                res.push(j);
-            }
+    // create an object to store the key value pairs 
+    let numObj = {};
+    // iterate through the nums array
+    for(let i = 0; i < nums.length; i++) {
+        // store the number into currNum
+        let currNum = nums[i];
+        // store the index as a value for the key at currNum
+        numObj[currNum] = i;
+    }
+    // now iterate through nums 
+    for(let i = 0; i < nums.length; i++) {
+        // find the difference between target and the current number being viewed
+        let diff = target-nums[i];
+        // find if the difference is in the object 
+        if(numObj.hasOwnProperty(diff) && i !== numObj[diff]) {
+            return [i, numObj[diff]];
         }
     }
-    return res;
 };
