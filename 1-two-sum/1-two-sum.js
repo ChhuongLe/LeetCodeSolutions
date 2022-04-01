1fa5ef6594dfb12834280ed5af01b1972a5b44e5
Chhuong Le <77046302+ChhuongLe@users.noreply.github.com>
2022-04-01 00:55:31 -0700

    Time: 85 ms (77.50%), Space: 45.6 MB (5.78%) - LeetHub

--- 1-two-sum/1-two-sum.js
+++ 1-two-sum/1-two-sum.js
@@ -3,31 +3,24 @@
  * @param {number} target
  * @return {number[]}
  */
-/*
-IOCE:
-    Input: nums - array of integers, target - integer
-    Output: Array of integers
-    Constraints: nums will always have a length >= 2
-    Edge Cases: None to consider at the moment
-*/
 var twoSum = function(nums, target) {
-    // create an object to store the key value pairs 
-    let numObj = {};
-    // iterate through the nums array
-    for(let i = 0; i < nums.length; i++) {
-        // store the number into currNum
-        let currNum = nums[i];
-        // store the index as a value for the key at currNum
-        numObj[currNum] = i;
-    }
-    // now iterate through nums 
-    for(let i = 0; i < nums.length; i++) {
-        // find the difference between target and the current number being viewed
-        let diff = target-nums[i];
-        // find if the difference is located in the object and if the current number is unique 
-        if(numObj.hasOwnProperty(diff) && i !== numObj[diff]) {
-            // if it is, return the 2 numbers that sum up to it
-            return [i, numObj[diff]];
+    // use hash to store number and index
+    let hash = {};
+    for (let i = 0; i < nums.length; i++){
+        if(!hash[nums[i]]){
+            hash[nums[i]] = i;
         }
     }
+    // hash of nums and element number is now initialized
+    // iterate again and find if the diff is present in the hash
+    let diff = 0;
+    let res = [];
+    for (let i = 0; i < nums.length; i++){
+        diff = target - nums[i];
+        if(hash[diff] && i !== hash[diff]){
+            res.push(i, hash[diff]);
+            break;
+        }
+    }
+    return res;
 };
