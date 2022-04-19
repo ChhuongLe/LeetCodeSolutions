ed3564235a056c7b640a756694b2a9b849e289d5
Chhuong Le <77046302+ChhuongLe@users.noreply.github.com>
2022-04-19 16:43:41 -0700

    Time: 564 ms (11.30%), Space: 50.2 MB (38.26%) - LeetHub

--- 694-number-of-distinct-islands/694-number-of-distinct-islands.js
+++ 694-number-of-distinct-islands/694-number-of-distinct-islands.js
@@ -4,67 +4,61 @@
  */
 
 /*
-    algorithm: (Brute force) 
-        1. traverse through the grid
-        2. when a '1' is found
-            2a. perform a DFS and check for other '1's in the area 
-            2b. Store the coordinates of the '1's and continue traversing until no adjacent '1's are found
-        3. translate the coordinates that were stored to a common reference point (top left)
-            3a. check if there are matching patterns 
-            3b. if not, increment the unique counter
-        4. return unique island count
-*/
-/*
-    algorithm: (save path traveled)
-        1. traverse through the grid
-        2. when a '1' is found
-            2a. perform a DFS and check for other '1's in the area 
-            2b. indicate whether it was an "up", "down", "left", or "right" direction and store it into an array
-            2c. return the array back to the main function
-        3. check if the current island's shape is present in the array
-            3a. if it is, then we can ignore it since a set only stores unique values
-            3b. if it is not, then we update the set with the new island shape and increment the uniqueIsland counter
-        4. return the total number of unique island shapes
+    algorithm: 
+        1. traverse through the matrix
+        2. search for a 1
+        3. when a 1 is reached, perform a DFS on the current location
+            3a. check for cases that fall outside of the matrix
+            3b. otherwise, flip the 1 to 0 to label that it has been checked alreadt
+            3c. perform a DFS up, down, left, and right until all islands have been checked 
+                3ca. While doing this append an array that records the path traveled
+        4. compare the path traveled with a set of paths
+            4a. if it is unique, increment the unique counter
+        5. return the number of unique islands
 */
 
 const DFS = (grid, r, c, path) => {
-    // perform a DFS from the grid
-    // check for one of these conditions
+    // check for out of bounds
     if(r < 0 || r > grid.length-1 || c < 0 || c > grid[0].length-1 || grid[r][c] === 0) {
-        return; 
-    }
-    // flip the sign from 1 to 0 to indicate that the location has been checked
+        // if any of the above conditions are met, do nothing
+        return;
+    } 
+    // otherwise, flip the 1 to 0 to indicate that this location has bee n visitied 
     grid[r][c] = 0;
-    // perform the checks 
-    let directions = [[-1, 0, 'U'], [1, 0, 'D'], [0, 1, 'R'], [0, -1, 'L']];
+    
+    // perform a dfs on multiple directions and associate the path with a letter representing it
+    let directions = [[-1, 0, 'U'], [1, 0, 'D'], [0, -1, 'L'], [0, 1, 'R']];
     for(let dir of directions) {
-        // push the direction in
         path.push(dir[2]);
-        // continue to search
-        DFS(grid, r+dir[0], c+dir[1], path);
+        DFS(grid, r + dir[0], c + dir[1], path);
     }
-    // join the array to form a single string
     return path.join("");
 };
 
 const numDistinctIslands = (grid) => {
-    // initialize a count for unique islands
+    // initialize a set to store the unquie paths 
+    let pathSet = new Set();
+    // initialize a variable to store unique paths
     let unique = 0;
-    let pathSets = new Set();
-    // traverse the grid
-    const r = grid.length, c = grid[0].length;
-    for(let i = 0; i < r; i++) {
-        for(let j = 0; j < c; j++) {
-            // if a '1' is found
+    
+    // traverse through the grid
+    const row = grid.length, col = grid[0].length;
+    for(let i = 0; i < row; i++) {
+        for (let j = 0; j < col; j++) {
+            // if grid at row i and col j is 1
             if(grid[i][j] === 1) {
-                // perform a DFS
+                // perform a DFS on it and store the returned array into a variable
                 let path = DFS(grid, i, j, []);
-                if(!pathSets.has(path)) {
-                    pathSets.add(path);
+                // check if the path is present in the set
+                if(!pathSet.has(path)) {
+                    // if it is not present in the set, increment the unique counter
                     unique++;
+                    // add it to the pathSet
+                    pathSet.add(path);
                 }
             }
         }
     }
+    // return the number of unique island shapes;
     return unique;
 };
