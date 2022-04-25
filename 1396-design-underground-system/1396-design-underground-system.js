1a7b3a61f99b2b10ab3d4d3c7bd4bf4e2693eba9
Chhuong Le <77046302+ChhuongLe@users.noreply.github.com>
2022-04-24 18:50:50 -0700

    Time: 235 ms (82.59%), Space: 56.3 MB (97.95%) - LeetHub

--- 1396-design-underground-system/1396-design-underground-system.js
+++ 1396-design-underground-system/1396-design-underground-system.js
@@ -1,7 +1,9 @@
-
 var UndergroundSystem = function() {
+    // store {id: [stationName, time]}
     this.arrivals = new Map();
-    this.avgs = new Map();
+    // avgs map
+    // store {A,B: [numPeople, totalTime]}
+    this.avg = new Map();
 };
 
 /** 
@@ -11,10 +13,11 @@ var UndergroundSystem = function() {
  * @return {void}
  */
 
-// takes a unique customer ID, stationName, and the time they arrived 
+/*
+    takes customer ID, stationName, and time checked in
+*/
 UndergroundSystem.prototype.checkIn = function(id, stationName, t) {
-    // id: [stationName, t] 
-    this.arrivals.set(id, [stationName, t]);
+    this.arrivals.set(id,[stationName, t]);
 };
 
 /** 
@@ -24,26 +27,30 @@ UndergroundSystem.prototype.checkIn = function(id, stationName, t) {
  * @return {void}
  */
 
-// takes a unique ID, stationName, and the time they checked out of the station
-// find the start station and store it into to referencable variables
-
+/*
+    from here, create unique key using the start station and end station
+    calculate the difference between endTime and startTime
+    update if the map already contains the unique key, otherwise initialize the new start and end stations with their respective values
+*/
 UndergroundSystem.prototype.checkOut = function(id, stationName, t) {
-    const [startStation, time] = this.arrivals.get(id);
+    // store the startStation name and startTime 
+    let [startStation, startTime] = this.arrivals.get(id);
+    // calculate the difference current t - startTime
+    let diff = t - startTime;
+    // make a unique key to be easily referenced to
+    let key = [startStation, stationName].join(",");
     
-    // find the difference of time between end station and start station
-    let diff = t - time;
-    
-    let key = [startStation, stationName].join(',');
-    // check if avgs map contains the unique key 
-    if(this.avgs.has(key)){
-        // update the totalTime and numPeople if the map already contains the key
-        let [totalTime, numPeople] = this.avgs.get(key);
-        totalTime += diff;
+    // check average map to see if the key already exsists
+    if(this.avg.has(key)){
+        let [numPeople, totalTime] = this.avg.get(key);
         numPeople++;
-        this.avgs.set(key, [totalTime, numPeople]);
+        totalTime += diff;
+        
+        this.avg.set(key,[numPeople, totalTime]);
     } else {
-        this.avgs.set(key, [diff, 1]);
+        this.avg.set(key, [1, diff]);
     }
+    this.arrivals.delete(id);
 };
 
 /** 
@@ -51,13 +58,17 @@ UndergroundSystem.prototype.checkOut = function(id, stationName, t) {
  * @param {string} endStation
  * @return {number}
  */
+
+// average = totalTime/numPeople
 UndergroundSystem.prototype.getAverageTime = function(startStation, endStation) {
     let key = [startStation, endStation].join(',');
-    
-    if(this.avgs.has(key)){
-        const [totalTime, numPeople] = this.avgs.get(key);
-        return totalTime/numPeople;
+    if(!this.avg.has(key)){
+        return null;
+    } else {
+        let [numPeople, totalTime] = this.avg.get(key);
+        return (totalTime/numPeople);
     }
+
 };
 
 /** 
