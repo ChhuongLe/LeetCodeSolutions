2524a29f4d4abb711d2f7f506b456e9f45aff5f4
Chhuong Le <77046302+ChhuongLe@users.noreply.github.com>
2022-04-28 12:35:00 -0700

    Time: 102 ms (90.72%), Space: 47.8 MB (50.65%) - LeetHub

--- 445-add-two-numbers-ii/445-add-two-numbers-ii.js
+++ 445-add-two-numbers-ii/445-add-two-numbers-ii.js
@@ -12,60 +12,51 @@
  */
 
 /*
-    IOCE: 
-        Input: l1 & l2 - 2 linked lists 
-        Output: linked list
-        Constraints: Will always has a node
-        Edge Cases: Carry over at the end where there is nothing to add left
-    
     Algorithm: 
-        1. reverse the list (reverse the list to add the numbers from the 1's position and then move forward)
-        2. Initalize the result list and the carry
-        3. traverse through the list until we reach both the ends of the list
-            3a. if an end is reached, set their numerical value to be added to be 07y
-            3e. move to the next items in the list
+        1. Reverse the list so that we can add the ones digits first 
+        2. traverse through the 2 lists until they're empty
+            2b. add the two numbers, if its over 10, add it to the carry 
+            2b. add the carry and the sum and add it to the total node
+        3. return the final list 
 */
 
-const reverseList = (head) => {
-    let lastNode = null;
+const reverse = (node) => {
+    let last = null;
     
-    while(head) {
-        let tmp = head.next;
-        head.next = lastNode;
-        lastNode = head;
-        head = tmp;
+    while(node) {
+        let temp = node.next;
+        node.next = last;
+        last = node;
+        node = temp;
     }
-    // return list
-    return lastNode;
+    return last;
 };
 
 const addTwoNumbers = (l1, l2) => {
-    // first reverse the list
-    l1 = reverseList(l1);
-    l2 = reverseList(l2);
+    l1 = reverse(l1);
+    l2 = reverse(l2);
+    let carry = 0, resList = null;
     
-    // initialize a resulting list and a carry for the carry overs
-    let resList = null, carry = 0;
-    // traverse both lists while they have something in them
     while(l1 || l2) {
-        // check if the list has reached an end
-        let n1 = l1 !== null ? l1.val : 0;
-        let n2 = l2 !== null ? l2.val : 0;
+        // check if the value of l1 is null, if it isnt then store the value, otherwise store it as 0
+        let val1 = l1 !== null ? l1.val : 0;
+        let val2 = l2 !== null ? l2.val : 0;
         
-        // calculate the sum of the two numbers by adding the two + the carry and modding it by 10 to get a single 1's digit
-        let sum = Math.floor(carry + n1 + n2)%10;
-        carry = Math.floor(carry + n1 + n2)/10;
+        // if a decimal value appears, then round down to the nearest integer value
+        let sum = Math.floor(carry + val1 + val2)%10;
+        carry = Math.floor(carry + val1 + val2)/10; 
         
-        // update value by adding to the front of the list
+        // update the resList
         let curr = new ListNode(sum);
         curr.next = resList;
         resList = curr;
         
-        // move along the lists 
+        // move the list forward
         l1 = l1 !== null ? l1.next : null;
         l2 = l2 !== null ? l2.next : null;
     }
-    // edge case where the carry over is at the end and did not get calculated
+    
+    // edge case where the carry is found at the very end
     if(Math.floor(carry) !== 0) {
         let curr = new ListNode(Math.floor(carry));
         curr.next = resList;
