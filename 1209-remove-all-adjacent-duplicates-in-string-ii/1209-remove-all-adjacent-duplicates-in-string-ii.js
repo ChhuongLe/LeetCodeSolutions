a636a23dfc376d4879c515e38da5daa7a28aa44f
Chhuong Le <77046302+ChhuongLe@users.noreply.github.com>
2022-04-30 09:58:16 -0700

    Time: 771 ms (13.44%), Space: 52.3 MB (27.56%) - LeetHub

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.js
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.js
@@ -3,41 +3,32 @@
  * @param {number} k
  * @return {string}
  */
+
 /*
-    k - represents the duplicates to remove
-        i.e if k = 3 "aa" will not be removed but "aaa" will be
-        
-        algorithm: 
-            1. iterate through the string s
-            2. push the number of times the same letter appears in the stack
-                2a. when the number hits k, pop the number of letters out of the stack
-            3. continue iterating through the stack and see if the next letter is the same as the previous
-                3a. if it is, increment the count of that letter
-                3b. otherwise initialize the value to 1
-            4. return the final result
+    given a string s and integer k
+    string needs to repeat k times before it can be removed
+    
+    algorithm: 
+        1. initialize a stack and a count variable
+        2. traverse through the string
+            2a. if the current character is the same as before, increment the top of the stack
+            2b. else, push 1 to the stack
+        3. if the count is the same as k, pop k characters from the stack
+        4. return the final stack joined
 */
 var removeDuplicates = function(s, k) {
-    // initialize the stack
     let stack = [];
-    // iterate through the string
     s = s.split('');
-    for(let i = 0; i < s.length; i++){
-        // if we're at the beginning or if the char at i is different from the char previously, push to stack 1
-        if(i === 0 || s[i] !== s[i-1]){
-            stack.push(1);
-        } else {
-            // otherwise a match is found and increment the value at the top of the stack
+    for (let i = 0; i < s.length; i++) {
+        if(s[i-1] !== undefined && s[i-1] === s[i]) {
             stack[stack.length-1]++;
-            
-            // check if the top of the stack is equal to k
-            if(stack[stack.length-1] === k){
-                // pop it out of the stack
-                stack.pop();
-                // remove the duplicates from the string arr
-                s.splice(i-k+1, k);
-                // move i back k positions to continue checking for duplicates
-                i = i - k;
+            if(stack[stack.length-1] === k) {
+                let index = i-stack[stack.length-1]+1;
+                s.splice(index,stack.pop());
+                i = index-1;
             }
+        } else {
+            stack.push(1);
         }
     }
     return s.join('');
