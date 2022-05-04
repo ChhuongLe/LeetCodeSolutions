56f0a7e75add8e443ebe449e5f93094b52b8b607
Chhuong Le <77046302+ChhuongLe@users.noreply.github.com>
2022-05-04 14:43:16 -0700

    Time: 651 ms (71.44%), Space: 95.2 MB (98.30%) - LeetHub

--- 146-lru-cache/146-lru-cache.js
+++ 146-lru-cache/146-lru-cache.js
@@ -1,19 +1,31 @@
 /**
  * @param {number} capacity
  */
+
+/*
+    Need to know when capacity has been reached
+    Use a map because it stores the order that it was inserted in
+*/
 var LRUCache = function(capacity) {
-    this.cache = new Map();
     this.capacity = capacity;
+    this.cache = new Map();
 };
 
 /** 
  * @param {number} key
  * @return {number}
  */
+
+/*
+  if the key is not present in the cache, return -1
+  
+  if it does exist, store the value, and re-insert the key and value to update that it was recently used
+  return the value at the end
+*/
 LRUCache.prototype.get = function(key) {
-   if(!this.cache.has(key)){
-       return -1;
-   }
+    if(!this.cache.has(key)) {
+        return -1;
+    }
     const val = this.cache.get(key);
     this.cache.delete(key);
     this.cache.set(key, val);
@@ -26,10 +38,15 @@ LRUCache.prototype.get = function(key) {
  * @return {void}
  */
 
-// set the key and value into the map first
-// if the capacity is capped, remove the first item in the map
+/*
+    if the cache has the key already, delete it and then update the cache
+    check if the capacity is full
+        if it is then remove the top of the map
+*/
 LRUCache.prototype.put = function(key, value) {
-    this.cache.delete(key);
+    if(this.cache.has(key)){
+        this.cache.delete(key);
+    }
     this.cache.set(key, value);
     if(this.cache.size > this.capacity) {
         this.cache.delete(this.cache.keys().next().value);
