9a874dc587b707b028a2ab5e51f2c282e3fa7e26
Chhuong Le <77046302+ChhuongLe@users.noreply.github.com>
2022-12-02 10:25:12 -0800

    Update 2-add-two-numbers.js

--- 2-add-two-numbers/2-add-two-numbers.js
+++ 2-add-two-numbers/2-add-two-numbers.js
@@ -10,20 +10,29 @@
  * @param {ListNode} l2
  * @return {ListNode}
  */
+
 var addTwoNumbers = function(l1, l2) {
-    let node = null
-    const carry = arguments[2]
-    if (l1 || l2) {
-        const val1 = l1 ? l1.val : 0
-        const val2 = l2 ? l2.val : 0
-        const next1 = l1 ? l1.next : null
-        const next2 = l2 ? l2.next : null
-        const val = carry ? val1 + val2 + 1 : val1 + val2
-        node = new ListNode(val % 10)
-        node.next = addTwoNumbers(next1, next2, val >= 10)  
-    } else if (carry) {
-        node = new ListNode(1)
-        node.next = null
+    let dummy = new ListNode(0);
+    let curr = dummy;
+    let carry = 0;
+    // traverse through the list until `null` is found 
+    // if carry still has a value, at the end, add it to the result (i.e. if only  8 and 8 are passed in)
+    while (l1 || l2 || carry) {
+        let v1 = l1 ? l1.val : 0;
+        let v2 = l2 ? l2.val : 0;
+
+        // compute the new value
+        let sum = v1 + v2 + carry;
+        // if there is a carry
+        carry = Math.floor(sum/10);
+        curr.next = new ListNode(sum%10);
+
+        // update pointers
+        curr = curr.next;
+        l1 = l1 ? l1.next : null;
+        l2 = l2 ? l2.next : null;
     }
-    return node
+
+
+    return dummy.next;
 };
