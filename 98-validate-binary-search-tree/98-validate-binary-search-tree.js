5e45d6776bcb7108f96d5b396cd7046b82b2cf4e
Chhuong Le <77046302+ChhuongLe@users.noreply.github.com>
2022-12-02 23:22:39 -0800

    Update 98-validate-binary-search-tree.js

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.js
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.js
@@ -10,32 +10,19 @@
  * @param {TreeNode} root
  * @return {boolean}
  */
-
-// perform an inorder traversal
 var isValidBST = function(root) {
-    // initialize an empty arr to store the traversal path
-    let arr = [];
-    // function that performs inorder traversal
-    const inOrder = (node) => {
-        if(node === null){
-            return;
-        }
-        // go all the way left
-        inOrder(node.left);
-        // store the value into the array
-        arr.push(node.val);
-        // go all the way right
-        inOrder(node.right);
-        // return the final array
-        return arr;
+    let inOrderArr = [];
+    const inOrderTraversal = (node) => {
+        if(!node) return [];
+        return [...inOrderTraversal(node.left), node.val, ...inOrderTraversal(node.right)];
     }
-    // perform the traversal and store it into res
-    let res = inOrder(root, arr);
-    // loop through array and check if the numbers are in ascending order
-    for(let i = 1; i < arr.length; i++) {
-        if(arr[i-1] >= arr[i]){
-            return false;
-        }
+    inOrder = inOrderTraversal(root);
+
+    for(let i=1; i < inOrder.length; i++) {
+        let curr = inOrder[i], prev = inOrder[i-1];
+
+        if(prev >= curr) return false;
     }
+    
     return true;
 };
