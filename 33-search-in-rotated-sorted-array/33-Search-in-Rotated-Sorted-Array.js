94e8a4b5a307dbb4fe6c82b0800e1e737dcb8469
Chhuong Le <77046302+ChhuongLe@users.noreply.github.com>
2023-01-05 13:33:39 -0800

    Update 33-Search-in-Rotated-Sorted-Array.js

--- 33-search-in-rotated-sorted-array/33-Search-in-Rotated-Sorted-Array.js
+++ 33-search-in-rotated-sorted-array/33-Search-in-Rotated-Sorted-Array.js
@@ -5,56 +5,58 @@
  */
 
  /*
-    Use binary search to achieve O(log n) runtime complexity
+    IOCE: 
+        Input: nums - array of integers
+        Output: index - integer representing the index of where the target was found
+                        (must return -1 if the target is not found in the array)
+        Constraints: nums will never be empty
+                     all values of nums will be unique
+                     nums is possibly rotated and in ascending order
+        Examples: 
+            [4,5,6,7,0,1,2], target = 0
+            should return 4 since index 4 is where the target is located
 
-    example:
-    [4,5,6,7,0,1,2] tartget = 0
+            [4,5,6,7,0,1,2] target = 3
+            should return -1 since the target cannot be found in the array nums
 
-    start at middle: 7
-    0 < 7 can search [4,5,6] or [0,1,2]
-    check left side
-    0 < 4 left side is out since 4 would be the smallest number due to pre-sorting
-    check middle of right side
-    0 < 1 decrement position
-    0 == 0
-
-    return element index
+    Algorithm: Binary search
+        1. set up pointers for the left and right side
+        2. traverse through the nums array 
+            2a. initialize a midpoint
+            2b. check if mid is equiv to the target
+                2ba. if it is return the index
+            2c. check if the the target can be found in either the left or right side of the array
+        3. If the traversal comes out to be unsucessful, return -1
  */
-var search = function(nums, target) {
-    let l = 0, r = nums.length - 1;
+var search = function(nums, target){
+    let l = 0, r = nums.length-1;
 
-    while (l <= r) {
-        // initialize the middle index
+    while(l <= r) {
         let mid = Math.floor((l+r)/2);
-        
-        // if middle element is the target, return
-        if(nums[mid] === target) {
+        // check if the target has been found
+        if(target === nums[mid]){
             return mid;
         }
-
-        // check if left side of the array is sorted
-        if(nums[l] <= nums[mid]) {
-            // check if the target is greater than the middle element or if it is less than the first element
-            if (target > nums[mid] || target < nums[l]) {
-                // move left pointer to the right portion
-                l = mid + 1;
-            } else {
-                // otherwise move the r pointer into the left portion of the array
+        
+        // check if the left side of the array is sorted 
+        if (nums[l] <= nums[mid]){
+            // check if the target can be located in the left side
+            if(nums[l] <= target && target <= nums[mid]){
+                // reset the right pointer
                 r = mid - 1;
+            } else {
+                // otherwise it will be in the right side and re-set the left side 
+                l = mid + 1;
             }
-        // check right side portion
+            // otherwise the right side is sorted
         } else {
-            // check if the target is less than the middle element or if target is greater than the last element in the right portion's array
-            if (target < nums[mid] || target > nums[r]) {
-                // move the right pointer towards the left side
-                r = mid - 1;
+            // check if the target can be found in the right side of the array
+            if(nums[mid] <= target && target <= nums[r]){
+                l = mid + 1
             } else {
-                // otherwise move the left pointer 
-                l = mid + 1;
+                r = mid - 1;
             }
         }
     }
-
-    // if the iteration ends without find the target, return -1
     return -1;
 };
