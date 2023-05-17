063b5483256240453c70acd1a93b08bb7402ae74
Chhuong Le <77046302+ChhuongLe@users.noreply.github.com>
2023-05-16 18:52:43 -0700

    Time: 61 ms (49.91%), Space: 42.2 MB (39.46%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.js
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.js
@@ -4,40 +4,57 @@
  * @return {boolean}
  */
 
+/*
+    Question asks us to find a solution that is of O(log(m*n)) complexity
+    
+    if we implement 2 binary searches to first find the row that the element could possibly be located in
+    then find the element itself within that row, this would fulfill the constraint
+    
+    First implement a function called findRow
+        1. initialize top and bottom variables to keep track of which rows we are on
+        2. traverse through the array
+        3. Isolate the "middle" row of the matrix
+        4. Compare the target to the start and end of the matrix
+            4a. if the target is less than the start -> move the bottom up
+            4b. if the target is greater than the end -> move the top down
+        5. If both checks fail, break out of the loop and return the index of the row
+        
+    Now we search through the array that we isolated using binary search using similar principles 
+*/
+
 const findRow = (matrix, target) => {
     let top = 0, bottom = matrix.length - 1;
+    let len = matrix[0].length - 1, mid;
     
-    let len = matrix[0].length - 1, row;
-    
-    while(top <= bottom) {
-        row = Math.floor((top+bottom)/2);
-        
-        if(target < matrix[row][0]) {
-            bottom = row - 1;
-        } else if(target > matrix[row][len]) {
-            top = row + 1;
+    while (top <= bottom) {
+        mid = Math.floor((top+bottom)/2);
+        if(target < matrix[mid][0]) {
+            bottom = mid - 1;
+        } else if (target > matrix[mid][len]) {
+            top = mid + 1;
         } else {
+            // break out if number may be in the row
             break;
         }
     }
-    return row;
+    // return the index of the row 
+    return mid;
 }
 
 var searchMatrix = function(matrix, target) {
     let row = findRow(matrix, target);
     let arr = matrix[row];
     
-    let l = 0, r = arr.length - 1;
+    let left = 0, right = arr.length - 1, mid;
     
-    while(l <= r) {
-        let mid = Math.floor((l+r)/2);
-        
+    while (left <= right) {
+        mid = Math.floor((left + right)/2);
         if(target === arr[mid]) {
             return true;
         } else if (target < arr[mid]) {
-            r = mid - 1;
+            right = mid - 1;
         } else {
-            l = mid + 1;
+            left = mid + 1;
         }
     }
     return false;
