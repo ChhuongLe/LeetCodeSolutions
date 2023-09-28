fc543d1f3639ceceac63803c7758a59028234309
Chhuong Le <77046302+ChhuongLe@users.noreply.github.com>
2023-09-28 14:17:31 -0700

    Time: 99 ms (85.77%), Space: 54.8 MB (26.5%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.js
+++ 0049-group-anagrams/0049-group-anagrams.js
@@ -3,29 +3,13 @@
  * @return {string[][]}
  */
 var groupAnagrams = function(strs) {
-    let sortedStr = [], hash = {}, i = 0, res = [];
+    // optimiezed solution
+    let hash = {};
 
-    for(const s of strs) {
-        // push a sorted string into the sortedStr array (i.e. NAT -> ANT & TAN -> ANT)
-        sortedStr.push(s.split('').sort().join());
+    for (let str of strs) {
+        let sortedStr = str.split('').sort().join('');
+        if(!hash[sortedStr]) hash[sortedStr] = [];
+        hash[sortedStr].push(str)
     }
-
-    // now that we have a sorted array, compare the matches and store the indicies
-    for(const s of sortedStr) {
-        if(!hash[s]) {
-            hash[s] = [i];
-        } else {
-            hash[s].push(i);
-        }
-        i++;
-   } 
-
-   for(const key in hash) {
-       let subArr = [];
-       for(let idx of hash[key]) {
-           subArr.push(strs[idx]);
-       }
-       res.push(subArr);
-   }
-   return res;
+    return Object.values(hash);
 };
