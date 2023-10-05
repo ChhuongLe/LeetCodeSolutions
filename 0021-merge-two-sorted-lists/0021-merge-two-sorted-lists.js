861a38aa5d75c04d96e41f20e75b36c784fac17c
Chhuong Le <77046302+ChhuongLe@users.noreply.github.com>
2023-10-04 20:49:36 -0700

    Time: 71 ms (7.34%), Space: 44.1 MB (45.61%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
@@ -11,19 +11,18 @@
  * @return {ListNode}
  */
 var mergeTwoLists = function(list1, list2) {
-    let head = new ListNode(null);
-
-    let curr = head;
-    while(list1 && list2) {
-        if(list1.val < list2.val) {
-            curr.next = list1;
-            list1 = list1.next;
-        } else {
-            curr.next = list2;
-            list2 = list2.next;
-        }
-        curr = curr.next;
+    if(!list1 || !list2) {
+        return list1 || list2;
     }
-    curr.next = list1 || list2;
-    return head.next;
+
+    let node = null;
+    if(list1.val < list2.val) {
+        node = list1;
+        node.next = mergeTwoLists(list1.next, list2);
+    } else {
+        node = list2;
+        node.next = mergeTwoLists(list1, list2.next);
+    }
+
+    return node;
 };
