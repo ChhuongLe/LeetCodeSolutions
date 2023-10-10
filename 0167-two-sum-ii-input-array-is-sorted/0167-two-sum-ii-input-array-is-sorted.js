74cfadf8c77bebc7cd8de13c15c42f98bb1eda35
Chhuong Le <77046302+ChhuongLe@users.noreply.github.com>
2023-10-10 12:17:04 -0700

    Time: 47 ms (93.23%), Space: 43.1 MB (27.34%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.js
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.js
@@ -3,20 +3,29 @@
  * @param {number} target
  * @return {number[]}
  */
+
+ // update from using hash
+ // use two pointers, one from the start, one from the end
+    // check if the sum is equal to the target
+    // if sum is larger, that means that the right number is too large
+        // decrement right
+    // if sum is smaller, left number is too little
+        // increment left
+// return left and right
 var twoSum = function(numbers, target) {
-    let hash = {}, i = 0;
-    for(const num of numbers) {
-        hash[num] = i;
-        i++;
-    }
-    let res = [], j = 0;
-    for(const num of numbers) {
-        let diff = target - num;
-        if(hash[diff] && j !== hash[diff]) {
-            res.push(j+1, hash[diff]+1);
-            break;
+    let l = 0, r = numbers.length-1;
+
+    while(l < r) {
+        let sum = numbers[l] + numbers[r];
+
+        if(target === sum){ 
+            return [l + 1, r + 1];
         }
-        j++
+
+        if(sum > target) {
+            r--;
+        } else {
+            l++;
+        }
     }
-    return res;
 };
