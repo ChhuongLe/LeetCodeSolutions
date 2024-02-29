2b9b3336494cd202d35397f3c00848423c94a6bd
Chhuong Le <77046302+ChhuongLe@users.noreply.github.com>
2024-02-29 07:37:44 -0800

    Time: 52 ms (69.58%) | Memory: 48.8 MB (45.78%) - LeetSync

--- 2436-make-array-zero-by-subtracting-equal-amounts/make-array-zero-by-subtracting-equal-amounts.js
+++ 2436-make-array-zero-by-subtracting-equal-amounts/make-array-zero-by-subtracting-equal-amounts.js
@@ -12,31 +12,31 @@
 
     knowing this now, we can look at the number of unique integers and count how many steps to get to all zero thorugh that
 */
-// var minimumOperations = function(nums) {
-//     const len = nums.length;
-//     // sort the integers in ascending order
-//     nums.sort((a,b)=> a-b);
-//     // initialize number of steps
-//     let steps = 0;
+var minimumOperations = function(nums) {
+    const len = nums.length;
+    // sort the integers in ascending order
+    nums.sort((a,b)=> a-b);
+    // initialize number of steps
+    let steps = 0;
 
-//     // traverse through the array
-//     for(let i = 0; i < len; i++) {
-//         // if nums at i is 0, skip the traversal
-//         if(nums[i] === 0) {
-//             continue;
-//         }
-//         // increment steps
-//         steps++;
-//         // subtract current element from all subsequent positives as it is already sorted
-//         for(let j = i + 1; j < len; j++) {
-//             nums[j] -= nums[i]
-//         }
-//     }
-//     return steps;
-// };
+    // traverse through the array
+    for(let i = 0; i < len; i++) {
+        // if nums at i is 0, skip the traversal
+        if(nums[i] === 0) {
+            continue;
+        }
+        // increment steps
+        steps++;
+        // subtract current element from all subsequent positives as it is already sorted
+        for(let j = i + 1; j < len; j++) {
+            nums[j] -= nums[i]
+        }
+    }
+    return steps;
+};
 
 // greedy solution
-const minimumOperations = (nums) => {
+const minimumOperationsV2 = (nums) => {
     const set = new Set(nums);
 
     return set.size - (set.has(0) ? 1 : 0);
